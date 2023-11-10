69fca7c185eedcb485758aa51f34975389574df3
AVINASH MAHADEV YEROLKAR <avinashmyerolkar@gmail.com>
2023-11-10 17:09:07 +0530

    api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,2 @@
-palm_api_key = "AIzaSyCL0xKiTPVLTNrFfIoYO8bf_rO8yMrf6tM"
-gpt_api_key="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"
+palm_api_key = "Paste your own api key by creating account"
+gpt_api_key="Paste your own api key by creating account on openai"
